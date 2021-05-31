3904b28d28e66009e514d20581592279b506f8d2
Umar Idrees <umar.static@gmail.com>
2021-05-31 22:48:53 +0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+// Scripts & Functions
+
+window.addEventListener('DOMContentLoaded', event => {
+    
+    // Scrollspy function
+    const sideNav = document.body.querySelector('#sideNav');
+    if (sideNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#sideNav',
+            offset: 74,
+        });
+    };
+
+    // Responsive navbar toggle function
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
